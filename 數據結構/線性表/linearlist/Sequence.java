130be9dc0ff51063b609c5e5f5dd84da9c90b9fa
kentanvictor <2030457926@qq.com>
2017-10-03 14:55:06 +0800

    java实现线性表

--- 數據結構/線性表/linearlist/Sequence.java
+++ 數據結構/線性表/linearlist/Sequence.java
@@ -1,21 +1,69 @@
 package com.dell.example.linearlist;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
  * Created by JohnnyTan on 2017/10/2.
  */
 public class Sequence {
+    public static void main(String[] args) {
+        int i;
+        SLType SL = new SLType();//定义顺序表变量
+        Data pdata;//定义结点保存引用变量
+        String key;//保存关键字
+        System.out.println("顺序表操作演示!\n");
+        SL.Init(SL);//初始化顺序表
+        System.out.println("初始化顺序表完成!\n");
+        Scanner input = new Scanner(System.in);
+        do //循环添加结点数据
+        {
+            System.out.println("输入添加的结点(学号 姓名 年龄):");
+            Data data = new Data();
+            data.key = input.next();
+            data.name = input.next();
+            data.age = input.nextInt();
+
+            if (data.age != 0) {
+                if (SL.SLAdd(SL, data) == 0) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        while (true);
+        System.out.println("\n順序表中的结点顺序为:\n");
+        SL.SLALL(SL);//显示所有结点数据
+
+        System.out.println("\n要取出的结点的序号:");
+        i = input.nextInt();
+        pdata = SL.SLFindByNum(SL, i);
+        if (pdata != null) {
+            System.out.println("第" + i + "个结点为:" + pdata.key + "," + pdata.name + "," + pdata.age + "\n");
+        }
+
+        System.out.println("\n" + "要查找的关键字:");
+        key = input.next();
+        i = SL.SLFindByCont(SL,key);
+        pdata = SL.SLFindByNum(SL,i);
+        if(pdata != null)
+        {
+            System.out.println("第" + i+"个结点为:" + pdata.key+"," + pdata.name+"," +pdata.age+ ",");
+        }
+    }
+}
+
+class Data {
+    String key;
+    String name;
+    int age;
+}
+
+class SLType {
     static final int MAX = 100;
-
-    class Data {
-        String key;
-        String name;
-        int age;
-    }
-
-    class SLType {
-        Data[] ListData = new Data[MAX + 1];
-        int ListLen;
-    }
+    Data[] ListData = new Data[MAX + 1];
+    int ListLen;
 
     void Init(SLType SL) {
         SL.ListLen = 0;
@@ -82,4 +130,25 @@ public class Sequence {
         }
         return SL.ListData[n];
     }
+
+    int SLFindByCont(SLType SL, String key)//按照关键字查询结点
+    {
+        int i;
+        for (i = 1; i <= SL.ListLen; i++) {
+            if (SL.ListData[i].key.compareTo(key) == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    int SLALL(SLType SL)//显示顺序表中的所有的结点
+    {
+        int i;
+        for (i = 1; i <= SL.ListLen; i++) {
+            System.out.println(SL.ListData[i].key + "," + SL.ListData[i].name + "," + SL.ListData[i].age);
+        }
+        return 0;
+    }
 }
+
--- 數據結構/線性表/linearlist/SequenceList.java
+++
@@ -1,9 +0,0 @@
-package com.dell.example.linearlist;
-
-/*
- * Created by JohnnyTan on 2017/10/2.
- */
-public class SequenceList {
-    Sequence sq = new Sequence();
-}
-
