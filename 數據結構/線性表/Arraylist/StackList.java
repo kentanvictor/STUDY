b77ab32fae0e2e31d70a9d4d33db08e75ab151cf
kentanvictor <2030457926@qq.com>
2017-10-11 10:18:44 +0800

    StackList

--- 數據結構/線性表/Arraylist/StackList.java
+++ 數據結構/線性表/Arraylist/StackList.java
@@ -14,6 +14,31 @@ public class StackList {
         Scanner input = new Scanner(System.in);
         System.out.println("入栈操作:\n");
         System.out.println("输入姓名 年龄进行入栈操作:");
+        do {
+            Information info = new Information();
+            info.name = input.next();
+
+            if(info.name.equals("0"))
+            {
+                break;
+            }
+            else
+            {
+                info.age = input.nextInt();
+                st.PushST(stack,info);
+            }
+        }while (true);
+        String temp = "1";
+        System.out.println("出栈操作:按任意非0键进行出栈操作:");
+        temp = input.next();
+        while (!temp.equals("0"))
+        {
+            data = st.PopST(stack);
+            System.out.printf("出栈的数据是(%s,%d)\n",data.name,data.age);
+            temp = input.next();
+        }
+        System.out.println("测试结束!");
+        st.STFree(st);//释放所占用的空间
     }
 }
 class Information
